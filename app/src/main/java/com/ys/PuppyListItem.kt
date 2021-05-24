d876f671f8539a7667edc49243666053e7aede4a
YS <yoonsam86@gmail.com>
2021-05-24 11:15:24 +0900

    add Card style for PuppyListItem

--- app/src/main/java/com/ys/PuppyListItem.kt
+++ app/src/main/java/com/ys/PuppyListItem.kt
@@ -4,24 +4,35 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PuppyListItem(puppy: Puppy) {
-    Row {
-       Column(
-           modifier = Modifier
-               .padding(16.dp)
-               .fillMaxWidth()
-               .align(Alignment.CenterVertically)
-       ) {
-           Text(text = puppy.title, style = typography.h6)
-           Text(text = puppy.description, style = typography.caption)
-       }
+    Card(
+        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        elevation = 2.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ) {
+        Row {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = puppy.title, style = typography.h6)
+                Text(text = puppy.description, style = typography.caption)
+            }
+        }
     }
 }
