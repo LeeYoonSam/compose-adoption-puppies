eba783e701ab341c520387dac355a5c7c31a9f0c
YS <yoonsam86@gmail.com>
2021-05-24 11:22:33 +0900

    add puppies iamge in PuppyListItem

--- app/src/main/java/com/ys/PuppyListItem.kt
+++ app/src/main/java/com/ys/PuppyListItem.kt
@@ -1,9 +1,7 @@
 package com.ys
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,18 +10,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PuppyListItem(puppy: Puppy) {
     Card(
-        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
         elevation = 2.dp,
         backgroundColor = Color.White,
         shape = RoundedCornerShape(corner = CornerSize(16.dp))
     ) {
         Row {
+            PuppyImage(puppyId = puppy.puppyImageId)
             Column(
                 modifier = Modifier
                     .padding(16.dp)
@@ -35,4 +39,17 @@ fun PuppyListItem(puppy: Puppy) {
             }
         }
     }
+}
+
+@Composable
+private fun PuppyImage(puppyId: Int) {
+    Image(
+        painter = painterResource(id = puppyId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(8.dp)
+            .size(84.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+    )
 }
